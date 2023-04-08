4c536d8f26a8fea02a76bbd675db8228dcb47187
dongli <dongli@192.168.0.104>
2023-04-08 16:06:50 -0700

    add a functions

--- app/src/main/java/codeCoverage/App.java
+++ app/src/main/java/codeCoverage/App.java
@@ -9,8 +9,11 @@ public class App {
     }
 
     public static void main(String[] args) {
-
         System.out.println(new App().getGreeting());
 
+    }
+
+    public static int add(int a, int b) {
+        return a+b;
     }
 }
